5dc7800b29a190a4e6996c6ad001b6295bcb63a8
chan <cham9994@gmail.com>
2022-08-01 16:07:04 +0900

    🎉  config: 환경설정&패키지 설치

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2 className="underline">hello world</h2>
+        <p className="text-blue-400 text-center sm:bg-red-400 md:bg-green-300 lg:bg-blue-300">
+          hi im gyeongchan
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
