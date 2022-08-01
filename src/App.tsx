390448e62b245239c3870d188f4b98fcc61dbf52
chan <cham9994@gmail.com>
2022-08-01 17:04:37 +0900

    🎨  confg: 폰트 및 절대 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h2 className="underline">hello world</h2>
+        <h2 className="underline text-3xl font-jalnanche">hello world</h2>
         <p className="text-blue-400 text-center sm:bg-red-400 md:bg-green-300 lg:bg-blue-300">
           hi im gyeongchan
         </p>
